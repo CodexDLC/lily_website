ebb4ad4ba8e5983cc49143b320795454d808e566
CodexDLC <primecodex@gmail.com>
2026-04-25 21:12:14 +0200

    feat(migrations): add campaign tracking and client unsubscribe fields

--- src/lily_backend/system/migrations/0003_client_unsubscribe.py
+++ src/lily_backend/system/migrations/0003_client_unsubscribe.py
@@ -3,6 +3,13 @@ import uuid
 from django.db import migrations, models
 
 
+def generate_unique_unsubscribe_tokens(apps, schema_editor):
+    Client = apps.get_model("system", "Client")
+    for client in Client.objects.all():
+        client.unsubscribe_token = uuid.uuid4()
+        client.save(update_fields=["unsubscribe_token"])
+
+
 class Migration(migrations.Migration):
     dependencies = [
         ("system", "0002_catalogimportstate"),
@@ -10,6 +17,18 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AddField(
+            model_name="client",
+            name="unsubscribe_token",
+            field=models.UUIDField(
+                db_index=True,
+                default=uuid.uuid4,
+                editable=False,
+                null=True,
+                verbose_name="unsubscribe token",
+            ),
+        ),
+        migrations.RunPython(generate_unique_unsubscribe_tokens, reverse_code=migrations.RunPython.noop),
+        migrations.AlterField(
             model_name="client",
             name="unsubscribe_token",
             field=models.UUIDField(
